ba749cfc6628f0237b16dfdd372db36867647dc1
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2023-06-09 19:25:00 +0300

    Make classes global

--- addons/wyvernbox/editor/plugin.gd
+++ addons/wyvernbox/editor/plugin.gd
@@ -1,6 +1,126 @@
 @tool
 extends EditorPlugin
 
+const scripts_dir := "res://addons/wyvernbox/"
+const icons_dir := "res://addons/wyvernbox/icons"
+
+var added_scripts := [
+	[
+		"InventoryView",
+		preload(scripts_dir + "inventory_view.gd"),
+		icons_dir + "inventory_view.png",
+	],
+	[
+		"EquipBonus",
+		preload(scripts_dir + "equip_bonus.gd"),
+		icons_dir + "equip_bonus.png",
+	],
+	[
+		"ItemInstantiator",
+		preload(scripts_dir + "item_instantiator.gd"),
+		icons_dir + "item_instantiator.png",
+	],
+	[
+		"GrabbedItemStack",
+		preload(scripts_dir + "view_extra/grabbed_item_stack.gd"),
+		icons_dir + "grabbed_item_stack.png",
+	],
+	[
+		"ItemStackView",
+		preload(scripts_dir + "view_extra/item_stack_view.gd"),
+		icons_dir + "item_stack_view.png",
+	],
+	[
+		"Tooltip",
+		preload(scripts_dir + "view_extra/tooltip.gd"),
+		icons_dir + "tooltip.png",
+	],
+	[
+		"TooltipProperty",
+		preload(scripts_dir + "view_extra/tooltip_property.gd"),
+		icons_dir + "tooltip_property.png",
+	],
+	# ITEMLIKES
+	[
+		"ItemType",
+		preload(scripts_dir + "item_type.gd"),
+		icons_dir + "item_type.png",
+	],
+	[
+		"ItemPattern",
+		preload(scripts_dir + "crafting/item_pattern.gd"),
+		icons_dir + "item_pattern.png",
+	],
+	[
+		"ItemGenerator",
+		preload(scripts_dir + "crafting/item_generator.gd"),
+		icons_dir + "item_generator.png",
+	],
+	[
+		"ItemConversion",
+		preload(scripts_dir + "crafting/item_conversion.gd"),
+		icons_dir + "item_conversion.png",
+	],
+	# ITEMLIKES : EXTRA
+	[
+		"ItemGeneratorEquipmentBonus",
+		preload(scripts_dir + "extension/item_generator_equipment_bonus.gd"),
+		icons_dir + "item_generator.png",
+	],
+	[
+		"ItemPatternEquipStat",
+		preload(scripts_dir + "extension/item_pattern_equip_stat.gd"),
+		icons_dir + "item_pattern.png",
+	],
+	[
+		"ItemPatternHighlightStack",
+		preload(scripts_dir + "extension/item_pattern_highlight_stack.gd"),
+		icons_dir + "item_pattern.png",
+	],
+	[
+		"ItemPatternName",
+		preload(scripts_dir + "extension/item_pattern_name.gd"),
+		icons_dir + "item_pattern.png",
+	],
+	# INVENTORIES
+	[
+		"Inventory",
+		preload(scripts_dir + "inventories/inventory.gd"),
+		icons_dir + "inventory.png",
+	],
+	[
+		"GridInventory",
+		preload(scripts_dir + "inventories/grid_inventory.gd"),
+		icons_dir + "grid_inventory.png",
+	],
+	[
+		"CurrencyInventory",
+		preload(scripts_dir + "inventories/currency_inventory.gd"),
+		icons_dir + "currency_inventory.png",
+	],
+	[
+		"RestrictedInventory",
+		preload(scripts_dir + "inventories/restricted_inventory.gd"),
+		icons_dir + "restricted_inventory.png",
+	],
+	# GROUND
+	[
+		"GroundItemsManager",
+		preload(scripts_dir + "ground/ground_items_manager.gd"),
+		icons_dir + "ground_items_manager.png",
+	],
+	[
+		"GroundItemStackView2D",
+		preload(scripts_dir + "ground/ground_item_stack_view_2d.gd"),
+		icons_dir + "ground_item_stack_view_2d.png",
+	],
+	[
+		"GroundItemStackView3D",
+		preload(scripts_dir + "ground/ground_item_stack_view_3d.gd"),
+		icons_dir + "ground_item_stack_view_3d.png",
+	],
+]
+
 
 var inspector_plugins = [
 	load("res://addons/wyvernbox/editor/inspector_plugin_inventory_creator.gd").new(self),
@@ -9,8 +129,19 @@ var inspector_plugins = [
 
 
 func _enter_tree():
+	var editor_base_node := get_editor_interface().get_base_control()
 	for x in inspector_plugins:
 		add_inspector_plugin(x)
+
+	for x in added_scripts:
+		var x_icon = x[2]
+		if x_icon == null:
+			x_icon = x[1].get_instance_base_type()
+
+		if x_icon is StringName || x_icon is String:
+			x_icon = editor_base_node.get_theme_icon(x_icon, "EditorIcons")
+
+		add_custom_type(x[0], x[1].get_instance_base_type(), x[1], x_icon)
 
 	initialize_setting("input/menu_inventory", {
 		"deadzone" : 0.2,
@@ -46,6 +177,9 @@ func _enter_tree():
 func _exit_tree():
 	for x in inspector_plugins:
 		remove_inspector_plugin(x)
+
+	for x in added_scripts:
+		remove_custom_type(x[0])
 
 
 func initialize_setting(key, value):
